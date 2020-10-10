b0743eba05dac95f156bada5a8a1ef9af9354eb0
Simon Willison <swillison@gmail.com>
2020-10-10 16:15:36 -0700

    Latest sqlite-utils copied from homebrew-core

--- Formula/sqlite-utils.rb
+++ Formula/sqlite-utils.rb
@@ -2,11 +2,19 @@ class SqliteUtils < Formula
   include Language::Python::Virtualenv
   desc "CLI utility for manipulating SQLite databases"
   homepage "https://sqlite-utils.readthedocs.io/"
-  url "https://files.pythonhosted.org/packages/91/c8/c9caccfb59f9deac5e160a2d7a9c15e631d455cf27c48165189de644a8e3/sqlite-utils-2.15.1.tar.gz"
-  sha256 "ca71a92bb96304323edc5b0fd1130f93d0ffe5083cd5a99b67e596efafee7220"
+  url "https://files.pythonhosted.org/packages/6a/9d/42871b729018c48ecdf9f14f14e5f6fc99416a088b6040b32ce494528ddb/sqlite-utils-2.21.tar.gz"
+  sha256 "fdb3ac8a2ce7da4253a04d9e57b7a1bbb4c2d756416fd9ae5c4459002453edc7"
   license "Apache-2.0"
+  revision 1
 
-  depends_on "python@3.8"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "38300360a4392095c38eed4eda89615f294c0f0fadf93f9d2e012d55d43ae970" => :catalina
+    sha256 "560e78faa25f44d3b449ea2d33ea08ec3a7b632960d8dc6770198d28de6351ba" => :mojave
+    sha256 "0423bb96c1e370cf6c5c4e40860bd43aacff88b73da9aa89093efb933606529d" => :high_sierra
+  end
+
+  depends_on "python@3.9"
 
   resource "click" do
     url "https://files.pythonhosted.org/packages/27/6f/be940c8b1f1d69daceeb0032fee6c34d7bd70e3e649ccac0951500b4720e/click-7.1.2.tar.gz"
